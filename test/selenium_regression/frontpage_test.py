2efed22696378c39e6d4f242d5230fb6cf0a75b7
ariqchowdhury <ariqchowdhury@gmail.com>
2014-02-06 15:50:53 -0800

    added some ids to html to aid testing. Added a user login test

--- test/selenium_regression/frontpage_test.py
+++ test/selenium_regression/frontpage_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 import unittest
 
 IP_ADDRESS = 'http://localhost:8888'
@@ -38,11 +39,31 @@ class NewUserTest(unittest.TestCase):
 		self.browser.get(IP_ADDRESS)
 
 		# login to website using test account
-		
+		username_ele = self.browser.find_element_by_name("username")
+		password_ele = self.browser.find_element_by_name("password")
+		submit_login_ele = self.browser.find_element_by_id("signin")
+
+		self.assertIsNotNone(username_ele)
+		self.assertIsNotNone(password_ele)
+		self.assertIsNotNone(submit_login_ele)
+
+		username_ele.send_keys("Ironman")
+		password_ele.send_keys("jarvis")
+
+		submit_login_ele.click()
 
 		# check that the navbar displays the username and register
 		# form is removed
+		user_dropdown = self.browser.find_element_by_id("user_dropdown")
 
+		self.assertEqual(user_dropdown.text, "Ironman")
+
+		try:
+			register_form = self.browser.find_element_by_id("register_form")
+		except NoSuchElementException:
+			self.assertTrue(True)
+		else:
+			self.assertFalse(False)
 
 if __name__ == '__main__':
 	unittest.main()
