b536a42eb0b78dee472a928d7058a40171f08e51
ariqchowdhury <ariqchowdhury@gmail.com>
2014-02-06 14:24:46 -0800

    Added some test cases for frontpage content; changed the filename of the test to reflect the testcases. Added some id's to elements in html to aid testing.

---
+++ test/selenium_regression/frontpage_test.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+import unittest
+
+IP_ADDRESS = 'http://localhost:8888'
+
+class NewUserTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_title(self):
+		self.browser.get(IP_ADDRESS)
+
+		self.assertIn('Whatsup', self.browser.title)
+
+	def test_featured_list(self):
+		self.browser.get(IP_ADDRESS)
+
+		featured_list = self.browser.find_element_by_id("featured_list")
+		list_elements = featured_list.find_elements_by_class_name("channel_title")
+
+		for i, ele in enumerate(list_elements):
+			self.assertEqual("Channel %s" % i, list_elements[i].text)
+
+	def test_all_list(self):
+		self.browser.get(IP_ADDRESS)
+
+		all_list = self.browser.find_element_by_id("all_list")
+		list_elements = all_list.find_elements_by_class_name("channel_title")
+
+		for i, ele in enumerate(list_elements):
+			self.assertEqual("Channel %s" % i, list_elements[i].text)
+
+	def test_login(self):
+		self.browser.get(IP_ADDRESS)
+
+		# login to website using test account
+		
+
+		# check that the navbar displays the username and register
+		# form is removed
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+
