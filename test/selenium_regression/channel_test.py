25042184d16b8863373b496c1e395593e34fd96f
ariqchowdhury <ariqchowdhury@gmail.com>
2014-02-06 20:14:22 -0800

    working on channel test message sending

--- test/selenium_regression/channel_test.py
+++ test/selenium_regression/channel_test.py
@@ -2,7 +2,7 @@ import unittest
 
 from selenium import webdriver
 
-from login_helper import whatsup_login
+from login_helper import BaseWhatsupTester
 
 IP_ADDRESS = 'http://localhost:8888'
 
@@ -10,19 +10,32 @@ class ChannelTest(BaseWhatsupTester):
 
 	def sendMsg(self, msg):
 		comment_text = self.browser.find_element_by_id("comment")
+		comment_text.send_keys(msg)
+
+		send_button = self.browser.find_element_by_id("send")
+		send_button.click()
 
 	def test_send_message(self):
 		self.browser.get(IP_ADDRESS)
 
 		try:
-			whatsup_login(self.browser, "Ironman", "jarvis")
+			self.whatsup_login("Ironman", "jarvis")
 		except NoSuchElementException:
 			self.assertTrue(False)
 
 		featured_list = self.browser.find_element_by_id("featured_list")
 		list_elements = featured_list.find_elements_by_class_name("channel_title")
 
-		list_elements[0].click()
+		print list_elements
 
-		
+		# list_elements[0].click()
+
+		# sendMsg("short test")
+		# short_msg_box = self.browser.find_element_by_id("short_messages_grid")
+		# msg = short_msg_box.find_element_by_id("message_post_wrapper_1").find_element_by_id("message_post_msg").text
+		# print msg
+
+
+if __name__ == '__main__':
+	unittest.main()
 
